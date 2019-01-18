a59f573bf32ac0aa6ed944a6ffcb5ad4f1e514c3
Clinton <clintonjbeard@gmail.com>
2019-01-18 13:54:42 -0500

    Assignment completed

--- m1-05_Command_Line_Programs-pairs/src/main/java/com/techelevator/MakeChange.java
+++ m1-05_Command_Line_Programs-pairs/src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then display the change required.
  
@@ -8,10 +10,20 @@ package com.techelevator;
  Please enter the amount tendered: 100.00
  The change required is 76.35
  */
+
 public class MakeChange {
 
 	public static void main(String[] args) {
+		
+		Scanner in = new Scanner (System.in);
+		
+			System.out.println("Please enter the amount of the bill: ");
+			double amountOfTheBill = in.nextDouble();
+			
+			System.out.println("Please enter the amount tendered: ");
+			double amountOfTender = in.nextDouble();
 
+			System.out.println("The change required is: $" + (amountOfTender - amountOfTheBill));
 	}
 
 }
--- m1-05_Command_Line_Programs-pairs/src/main/java/com/techelevator/MartianWeight.java
+++ m1-05_Command_Line_Programs-pairs/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  In case you've ever pondered how much you weight on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -16,10 +18,20 @@ Enter a series of Earth weights (space-separated): 98 235 185
  235 lbs. on Earth, is 88 lbs. on Mars.
  185 lbs. on Earth, is 69 lbs. on Mars. 
  */
+
 public class MartianWeight {
 
 	public static void main(String[] args) {
-
+		
+		Scanner in = new Scanner (System.in);
+		
+		System.out.println("Please enter your Earth weight (in pounds): ");
+		double earthWeight = in.nextDouble();
+		
+		System.out.println("Here is your weight on Mars (in pounds): ");
+		double marsWeight = earthWeight * 0.378;
+		
+		System.out.println(earthWeight+ " lbs. on Earth, is "+marsWeight + " lbs. on Mars");
 	}
 
 }
